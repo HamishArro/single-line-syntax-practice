1a33fe71b039f5050cb6e86f22ecd57106d2dee7
Hamish Arro <Hamisharro@icloud.com>
2021-01-06 12:02:44 +0000

    add fizzbuzz for refactoring and changing into single line syntax

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,33 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "1" when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'FizzBuzz'
+  end
+end
+
+# IRB 1-100 Test: (1..100).each { |x| fizzbuzz(x) }
