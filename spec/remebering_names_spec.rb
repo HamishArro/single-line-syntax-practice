6f095602e5606998e3b9bcbc4e0d8919163c3529
Hamish Arro <Hamisharro@icloud.com>
2021-01-06 12:33:41 +0000

    make remebering_name_spec.rb pass by adding remebering_names.rb and giving it the correct method.

--- spec/remebering_names_spec.rb
+++ spec/remebering_names_spec.rb
@@ -1,5 +1,7 @@
-describe 'names' do
+require 'remebering_names'
 
-  it {names('Hamish').to eq "Name stored."}
+describe 'add_name' do
+
+  it { expect(add_name("Hamish")).to eq "Hamish stored." }
 
 end
