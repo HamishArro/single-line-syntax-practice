2955a3d0907f0d6fca63c7b1352e14df13f14176
Hamish Arro <Hamisharro@icloud.com>
2021-01-06 12:07:00 +0000

    refactor into single line syntax

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,26 +2,14 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'Fizz'
-  end
+  it { expect(fizzbuzz(3)).to eq 'Fizz' }
 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(5)).to eq 'Buzz'
-  end
+  it { expect(fizzbuzz(5)).to eq 'Buzz' }
 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(15)).to eq 'FizzBuzz'
-  end
+  it { expect(fizzbuzz(15)).to eq 'FizzBuzz'}
 
-  it 'returns "1" when passed 1' do
-    expect(fizzbuzz(1)).to eq 1
-  end
+  it { expect(fizzbuzz(1)).to eq 1 }
 
-  it 'returns "fizzbuzz" when passed 30' do
-    expect(fizzbuzz(30)).to eq 'FizzBuzz'
-  end
+  it {expect(fizzbuzz(30)).to eq 'FizzBuzz'}
 
 end
-
-# IRB 1-100 Test: (1..100).each { |x| fizzbuzz(x) }
